0b65ee33772a63c6df2d0ce11d9c4a7337976051
Tyler Morita <tyler.morita@m-six.com>
2019-08-23 15:26:08 -0700

    Finally fixed namespace stuff

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,4 @@
-// Type definitions for protractor-helpers v1.0.0
-// Project: https://github.com/wix/protractor-helpers
-// Definitions by: John Cant <https://github.com/johncant>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
-export {}
 declare global {
-    // ElementArrayFinder
 
     namespace protractor {
         interface ElementArrayFinder {
@@ -56,64 +49,64 @@ declare global {
             toBeKendoSelected(): boolean;
         }
     }
-
-    export function clickWhenClickable(htmlElement: protractor.ElementFinder, timoutInMilliseconds: number): void;
-
-    export function click(htmlElement: protractor.ElementFinder, timoutInMilliseconds: number): void;
-
-    export function tapWhenTappable(htmlElement: protractor.ElementFinder, timoutInMilliseconds: number): void;
-
-    export function tap(htmlElement: protractor.ElementFinder, timoutInMilliseconds: number): void;
-
-    export function fillFieldWithTextWhenVisible(htmlElement: protractor.ElementFinder, text: string, timoutInMilliseconds: number): void;
-
-    export function fillFieldWithText(htmlElement: protractor.ElementFinder, text: string, timoutInMilliseconds: number): void;
-
-    export function fillInputFieldWithFileWhenPresent(htmlElement: protractor.ElementFinder, absolutePath: string, timoutInMilliseconds: number): void;
-
-    export function uploadFileIntoInputField(htmlElement: protractor.ElementFinder, absolutePath: string, timoutInMilliseconds: number): void;
-
-    export function clearFieldWhenVisible(htmlElement: protractor.ElementFinder, timoutInMilliseconds: number): void;
-
-    export function clear(htmlElement: protractor.ElementFinder, timoutInMilliseconds: number): void;
-
-    export function clearFieldWhenVisibleAndFillItWithText(htmlElement: protractor.ElementFinder, text: string, timoutInMilliseconds: number): void;
-
-    export function clearFieldAndFillItWithText(htmlElement: protractor.ElementFinder, text: string, timoutInMilliseconds: number): void;
-
-    export function fillFieldWithTextWhenVisibleAndPressEnter(htmlElement: protractor.ElementFinder, text: string, timoutInMilliseconds: number): void;
-
-    export function fillFieldWithTextAndPressEnter(htmlElement: protractor.ElementFinder, text: string, timoutInMilliseconds: number): void;
-
-    // export function getBodyElementFromCurrentBrowserOrBrowserInstance(browserInstance) : protractor.ElementFinder;
-
-    // export function openNewBrowserInTheSamePage(browser) : ;
-
-    export function isCurrentUrlDifferentFromBaseUrl(): boolean;
-
-    export function scrollToElementWhenVisible(htmlElement: protractor.ElementFinder, timoutInMilliseconds: number): void;
-
-    export function scrollToElement(htmlElement: protractor.ElementFinder, timoutInMilliseconds: number): void;
-
-    export function setTimeout(timeoutInMilliseconds: number): void;
-
-    export function waitForElementPresence(htmlElement: protractor.ElementFinder, timoutInMilliseconds: number, errorMessage: string): void;
-
-    export function waitForElementNotToBePresent(htmlElement: protractor.ElementFinder, timoutInMilliseconds: number, errorMessage: string): void;
-
-    export function waitForElementVisibility(htmlElement: protractor.ElementFinder, timoutInMilliseconds: number, errorMessage: string): void;
-
-    export function waitForElementNotToBeVisible(htmlElement: protractor.ElementFinder, timoutInMilliseconds: number, errorMessage: string): void;
-
-    export function waitForTextToBePresentInElement(htmlElement: protractor.ElementFinder, text: string, timoutInMilliseconds: number, errorMessage: string): void;
-
-    export function waitForTextNotToBePresentInElement(htmlElement: protractor.ElementFinder, text: string, timoutInMilliseconds: number, errorMessage: string): void;
-
-    export function waitForUrlToBeEqualToExpectedUrl(expectedUrl: string, timoutInMilliseconds: number, errorMessage: string): void;
-
-    export function waitForUrlNotToBeEqualToExpectedUrl(expectedUrl: string, timoutInMilliseconds: number, errorMessage: string): void;
-
-    export function waitForUrlToContainString(string: string, timoutInMilliseconds: number, errorMessage: string): void;
-
-    export function waitForUrlNotToContainString(string: string, timoutInMilliseconds: number, errorMessage: string): void;
 }
+
+export function clickWhenClickable(htmlElement: protractor.ElementFinder, timoutInMilliseconds?: number): void;
+
+export function click(htmlElement: protractor.ElementFinder, timoutInMilliseconds?: number): void;
+
+export function tapWhenTappable(htmlElement: protractor.ElementFinder, timoutInMilliseconds?: number): void;
+
+export function tap(htmlElement: protractor.ElementFinder, timoutInMilliseconds?: number): void;
+
+export function fillFieldWithTextWhenVisible(htmlElement: protractor.ElementFinder, text: string, timoutInMilliseconds?: number): void;
+
+export function fillFieldWithText(htmlElement: protractor.ElementFinder, text: string, timoutInMilliseconds?: number): void;
+
+export function fillInputFieldWithFileWhenPresent(htmlElement: protractor.ElementFinder, absolutePath: string, timoutInMilliseconds?: number): void;
+
+export function uploadFileIntoInputField(htmlElement: protractor.ElementFinder, absolutePath: string, timoutInMilliseconds?: number): void;
+
+export function clearFieldWhenVisible(htmlElement: protractor.ElementFinder, timoutInMilliseconds?: number): void;
+
+export function clear(htmlElement: protractor.ElementFinder, timoutInMilliseconds?: number): void;
+
+export function clearFieldWhenVisibleAndFillItWithText(htmlElement: protractor.ElementFinder, text: string, timoutInMilliseconds?: number): void;
+
+export function clearFieldAndFillItWithText(htmlElement: protractor.ElementFinder, text: string, timoutInMilliseconds?: number): void;
+
+export function fillFieldWithTextWhenVisibleAndPressEnter(htmlElement: protractor.ElementFinder, text: string, timoutInMilliseconds?: number): void;
+
+export function fillFieldWithTextAndPressEnter(htmlElement: protractor.ElementFinder, text: string, timoutInMilliseconds?: number): void;
+
+// export function getBodyElementFromCurrentBrowserOrBrowserInstance(browserInstance) : protractor.ElementFinder;
+
+// export function openNewBrowserInTheSamePage(browser) : ;
+
+export function isCurrentUrlDifferentFromBaseUrl(): boolean;
+
+export function scrollToElementWhenVisible(htmlElement: protractor.ElementFinder, timoutInMilliseconds?: number): void;
+
+export function scrollToElement(htmlElement: protractor.ElementFinder, timoutInMilliseconds?: number): void;
+
+export function setTimeout(timeoutInMilliseconds: number): void;
+
+export function waitForElementPresence(htmlElement: protractor.ElementFinder, timoutInMilliseconds?: number, errorMessage?: string): void;
+
+export function waitForElementNotToBePresent(htmlElement: protractor.ElementFinder, timoutInMilliseconds?: number, errorMessage?: string): void;
+
+export function waitForElementVisibility(htmlElement: protractor.ElementFinder, timoutInMilliseconds?: number, errorMessage?: string): void;
+
+export function waitForElementNotToBeVisible(htmlElement: protractor.ElementFinder, timoutInMilliseconds?: number, errorMessage?: string): void;
+
+export function waitForTextToBePresentInElement(htmlElement: protractor.ElementFinder, text: string, timoutInMilliseconds?: number, errorMessage?: string): void;
+
+export function waitForTextNotToBePresentInElement(htmlElement: protractor.ElementFinder, text: string, timoutInMilliseconds?: number, errorMessage?: string): void;
+
+export function waitForUrlToBeEqualToExpectedUrl(expectedUrl: string, timoutInMilliseconds?: number, errorMessage?: string): void;
+
+export function waitForUrlNotToBeEqualToExpectedUrl(expectedUrl: string, timoutInMilliseconds?: number, errorMessage?: string): void;
+
+export function waitForUrlToContainString(string: string, timoutInMilliseconds?: number, errorMessage?: string): void;
+
+export function waitForUrlNotToContainString(string: string, timoutInMilliseconds?: number, errorMessage?: string): void;
